86124fa9ea0d183db179c674f251422ab828fe74
SyntaxError2505 <mr.sascha.cherevko@gmail.com>
2026-06-09 21:16:03 +0200

    got test.json to print with minor problems

--- src/main.c
+++ src/main.c
@@ -1,9 +1,62 @@
-#include <stdio.h>
-int main(int argc, char **argv){
-    char json_string[1024];
-    fgets(json_string, 1024, stdin);
+#include <string.h>
+#define INDENT_LENGTH 4
+#define BUFFER_LENGTH 1024
 
-    printf("%s\n", json_string);
+#include <stdio.h>
+
+static void print_indents(int indent_level){
+    for(int i = 0; i < indent_level * INDENT_LENGTH; i++){
+        printf(" ");
+    }
+}
+
+int main(int argc, char **argv){
+    char json_string[BUFFER_LENGTH];
+    fgets(json_string, BUFFER_LENGTH, stdin);
+
+    int indent_level = 0;
+    int json_length = strlen(json_string);
+
+    for(int i = 0; i < json_length; i++){
+        switch (json_string[i]) {
+            case '{':
+            printf("{\n");
+            indent_level++;
+            print_indents(indent_level);
+            break;
+
+            case '}':
+            printf("}\n");
+            indent_level--;
+            print_indents(indent_level);
+            break;
+
+            case '[':
+            printf("[\n");
+            indent_level++;
+            print_indents(indent_level);
+            break;
+
+            case ']':
+            printf("]\n");
+            indent_level--;
+            print_indents(indent_level);
+            break;
+
+            case ',':
+            printf(",\n");
+            print_indents(indent_level);
+            break;
+
+            case ':':
+            printf(": ");
+            break;
+
+            default:
+            printf("%c", json_string[i]);
+            break;
+        }
+    }
 
     return 0;
 }
