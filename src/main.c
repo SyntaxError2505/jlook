c8f9f865d90141c84d836de274b7760eb4478127
SyntaxError2505 <mr.sascha.cherevko@gmail.com>
2026-06-10 10:29:34 +0200

    added comment for readability

--- src/main.c
+++ src/main.c
@@ -66,7 +66,7 @@ int main(int argc, char **argv){
             break;
         } 
 
-        } else {
+        } else { // if(in_string_literal)
             if(json_string[i] == '"'){
                 in_string_literal = false;
             }
