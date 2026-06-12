b4b93f9fe6a1496f62afadc85c0dd117c0817805
SyntaxError2505 <mr.sascha.cherevko@gmail.com>
2026-06-12 23:15:27 +0200

    cleaned up main.c, removed harmless memory leak and added some error handling around string allocation

--- src/main.c
+++ src/main.c
@@ -1,17 +1,24 @@
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #include "PrintJson.h"
 #include "ReadPipe.h"
-#include "PrintChar.h"
 
 
 int main(int argc, char **argv){
     char* json_string = readPipe();
+    if(json_string == NULL){
+        fprintf(stderr, "JLOOK: ALLOCATING JSON FAILED, CHECK IF YOU HAVE ENOUGH RAM\n");
+        return -1;
+    }
 
     int indent_level = 0;
     bool in_string_literal = false;
 
     printJson(&in_string_literal, &indent_level, json_string);
+
+    free(json_string);
 
     return 0;
 }
