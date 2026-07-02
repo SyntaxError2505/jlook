fd70d00e9a483281b9149cb0f72ed5882ec3b753
SyntaxError2505 <mr.sascha.cherevko@gmail.com>
2026-07-02 07:56:03 +0200

    added gray color for coments

--- src/PrintChar.c
+++ src/PrintChar.c
@@ -7,6 +7,7 @@
 #define ANSI_COLOR_YELLOW "\x1b[33m"
 #define ANSI_COLOR_CYAN "\x1b[36m"
 #define ANSI_COLOR_ORANGE "\x1b[38;5;208m"
+#define ANSI_COLOR_GRAY "\033[90m"
 
 #define ANSI_COLOR_RESET "\x1b[0m"
 
@@ -34,6 +35,10 @@ void printChar(char c, enum Color color){
 
         case BRACKETS_3:
         printf("%s%c%s", ANSI_COLOR_ORANGE, c, ANSI_COLOR_RESET);
+        break;
+
+        case COMMENT:
+        printf("%s%c%s", ANSI_COLOR_GRAY, c, ANSI_COLOR_RESET);
         break;
 
         default:
